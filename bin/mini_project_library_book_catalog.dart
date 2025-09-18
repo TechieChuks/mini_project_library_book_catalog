3b343d6db89bd3ae5723570e403828f87b5360d2
TechieChuks <chuksanoliefo@gmail.com>
2025-09-18 19:33:51 +0100

    First Library Cat commit

---
+++ bin/mini_project_library_book_catalog.dart
@@ -0,0 +1,5 @@
+import 'package:mini_project_library_book_catalog/mini_project_library_book_catalog.dart' as mini_project_library_book_catalog;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mini_project_library_book_catalog.calculate()}!');
+}
---
+++ test/mini_project_library_book_catalog_test.dart
@@ -0,0 +1,8 @@
+import 'package:mini_project_library_book_catalog/mini_project_library_book_catalog.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
